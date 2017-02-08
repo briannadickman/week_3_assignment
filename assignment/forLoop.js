2dbc06de57f18321f8554f43de94b60e9e36bc72
Bri Dickman <brianna.dickman@gmail.com>
2017-02-08 16:56:48 -0600

    Created for loop to count number of shoes on the bus, logged 3 different counts

--- assignment/forLoop.js
+++ assignment/forLoop.js
@@ -1,6 +1,13 @@
 function shoesOnTheBus( kidsOnTheBus ){
+  for (var i = 0; i <= (kidsOnTheBus * 2); i +=2){
+      console.log(i);
+  }
   // this function accepts the number of kids on the bus
   // assume each kid on the bus is wearing 2 shoes
   // write a for loop that counts how many shoes are on the bus
   // this function should return the number of shoes on the bus
 } // end shoesOnTheBus
+
+shoesOnTheBus(12);
+shoesOnTheBus(6);
+shoesOnTheBus(45);
