0a167e7bf75e260a035b683a35f6e8da371213ab
Bri Dickman <brianna.dickman@gmail.com>
2017-02-08 16:47:10 -0600

    Completed isCartonFull function with if/else statement and added else if for overflowing carton

--- assignment/booleanConditions.js
+++ assignment/booleanConditions.js
@@ -1,4 +1,18 @@
-function isCartonFull( eggsInCarton, cartonCapacity ){
+
+function isCartonFull(eggsInCarton, cartonCapacity){
+    if (eggsInCarton == cartonCapacity) {
+      console.log(true);
+    }
+    else if (eggsInCarton > cartonCapacity) {
+      console.log('You have too many eggs!');
+    }
+    else {
+      console.log(false);
+    }
   // write an if statement checks if the carton is at cartonCapacity
   // this function should return true if so, false if not
 } // end checkIfCartonIsFull
+
+isCartonFull(10, 12);
+isCartonFull(15, 12);
+isCartonFull(12, 12);
